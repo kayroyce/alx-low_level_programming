d74fcedeee439b0b87764f4241d0cdac7e8efc84
kayroyce <kayroyce2005@gmail.com>
2022-04-08 15:37:21 -0700

    My puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ *main - entry point
  *
  *Return: 0 after printing the function
  */
 int main(void)
-[
-puts("\"programming is like building a multilingual puzzle")
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-]
+}
