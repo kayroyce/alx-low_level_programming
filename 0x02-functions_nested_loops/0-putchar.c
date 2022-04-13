f4e6b73d721692443bf23616603bcd1d5add76db
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-13 23:24:16 +0100

    My commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,10 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('K');
+	_putchar('O');
+	_putchar('L');
+	_putchar('A');
 	_putchar('\n');
 
 	return (0);
