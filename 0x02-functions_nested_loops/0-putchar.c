0bf2b85d62610120867cd1a1725a40e94b24fa77
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-13 15:38:54 +0100

    My commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,21 @@
-#include
-l
+#include "KOLAWOLE.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('K');
+	_putchar('O');
+	_putchar('L');
+	_putchar('A');
+	_putchar('W');
+	_putchar('O');
+	_putchar('L');
+	_putchar('E');
+	_putchar('\n');
+
+	return (0);
+}
