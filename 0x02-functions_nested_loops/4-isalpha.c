bc821de62090a8e733a0372c5a0de6c4a8dbb0bb
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-13 22:49:21 +0100

    My commit message

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,2 +1,16 @@
-iuy
-kj
+#include "main.h"
+
+/**
+  * _isalpha - Checks for alphabetic character
+  * The character to be checked
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
