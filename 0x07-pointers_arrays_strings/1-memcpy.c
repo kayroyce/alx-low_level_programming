efbab176f0188dfb48274ecf21bae41da96f29cd
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-26 20:37:34 +0100

    Memory copy commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,6 +2,10 @@
 
 /**
   * _memcpy - copy memory area
+  * @dest: dest memory area
+  * @src: source memory area
+  * @n: bytes from memory area to copy
+  *
   * Return: memory area replaced
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
