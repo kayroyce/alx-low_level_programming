b69c26d13944b5ccf61c9be4ee19ed94d614d3bc
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-26 20:20:23 +0100

    Memory set commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,16 @@
-Hi
+#include "main.h"
+
+/**
+  * Return: the memory area filled
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
