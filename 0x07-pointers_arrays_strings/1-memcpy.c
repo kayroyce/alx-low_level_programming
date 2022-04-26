7513904a592a67e8dd0ffb293b9947026e70c4e5
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-04-26 20:35:37 +0100

    Memory copy commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,17 @@
-Hi
+#include "holberton.h"
+
+/**
+  * _memcpy - copy memory area
+  * Return: memory area replaced
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	return (dest);
+}
