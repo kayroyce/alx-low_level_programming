2bbaf4df68657e56efd9f7d8a9efa7f5d067b433
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-05-17 07:39:04 +0100

    main commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-Readme for preprocessor project
+#include <stdio.h>
+
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
