420a7d1f0e7a49cab656ca1e4fb1fcefd6313ae7
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-05-17 21:59:45 +0100

    argument commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,16 @@
-readme file for argc
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints the number of args
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
