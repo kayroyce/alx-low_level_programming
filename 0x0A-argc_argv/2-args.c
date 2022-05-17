81d1e23257921dd9533afa71b66dc2c429ac96de
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-05-17 22:01:19 +0100

    Prints all the args content commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,21 @@
-readme file for argc
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints all the args content
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
