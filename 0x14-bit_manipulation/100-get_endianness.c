45075e53daa1c71ed3051c2f08f2c1141841d7dc
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-06-16 15:55:03 +0100

    correction

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if multibyte data is the endianness.
- *
- * Return: If big-endian - 0, if little-endian - 1.
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+	return (*c);
 }
