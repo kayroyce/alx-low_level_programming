b01ab34f23700c92ee44e4566bf399babc6e3154
kayroyce <kayroyce2005@gmail.com>
2022-07-08 00:30:29 -0700

    task

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,14 +5,15 @@
  * @head: head of node
  * Return: free'd list
  */
+
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp_node = NULL;
+	dlistint_t *remnant;
 
-	while (head != NULL)
+	while (head)
 	{
-		temp_node = head->next;
-		free(head);
-		head = temp_node;
+		remnant = head;
+		head = remnant->next;
+		free(remnant);
 	}
 }
