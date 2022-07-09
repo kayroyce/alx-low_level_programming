cbe9fa17b85d3fd34642ce720000d1d7c1d1bc44
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-07-09 01:58:59 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
-
 /**
  * free_dlistint - frees a doubly linked list
  * @head: double pointer to head
  */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *next;
-
 	while (head != NULL)
 	{
 		next = head->next;
