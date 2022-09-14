0dddcde72f2c8e146b2c2e002ff312d25efbb26f
kayroyce <kayroyce2005@gmail.com>
2022-09-14 19:26:02 +0100

    makefile task commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""island perimeter module
+"""
+def island_perimeter(grid):
+    """perimeter function
+    """ 
+
+    k = 0
+    length = len(grid) - 1
+    width = len(grid) - 1
+
+    for x, r in enumerate(grid):
+        for y, n in enumerate(grid):
+            if n == 1:
+                if x == 0 or grid[x - 1][y] != 1:
+                    k += 1
+                if y == 0 or grid[x][y - 1] != 1:
+                    k += 1
+                if y == width or grid[x][y + 1] != 1:
+                    k += 1
+                if x == width or grid[x + 1][y] != 1:
+                    k += 1
+
+    return k
+    
